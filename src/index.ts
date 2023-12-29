8ba22ad77b4a8541d0cc84710a7edb770cbc690f
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2023-12-28 23:53:14 -0300

    criando rota de user

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import  express from 'express'
 import tasksRoute from './routes/tasksRoute'
+import userRoute from './routes/userRoute'
+
 import cors from 'cors'
 const PORT = 3000
 
@@ -7,6 +9,8 @@ const app = express()
 app.use(express.json());
 app.use(cors())
 app.use("/tasks", tasksRoute)
+app.use("/user", userRoute)
+
 
 
 app.listen(PORT, () => {
