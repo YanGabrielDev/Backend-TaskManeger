50e8e130a38f9232e2bdd14a084e9bdce8372d8e
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2024-01-03 23:03:48 -0300

    criando arquivo de errors

---
+++ src/erros.ts
@@ -0,0 +1,11 @@
+export class ApiError extends Error {
+    statusCode: number = 500;
+  }
+  
+  export class BadRequestError extends ApiError {
+    statusCode = 400;
+  }
+  
+  export class DataNotFoundError extends ApiError {
+    statusCode = 404;
+  }
