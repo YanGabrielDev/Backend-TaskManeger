72799398dedf9d61d5e4a7485993711d406aac79
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2024-01-04 22:04:18 -0300

    configurando server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import  express from 'express'
+import express from 'express'
 import tasksRoute from './routes/tasksRoute'
 import userRoute from './routes/userRoute'
 
 import cors from 'cors'
+import { authMiddleware } from './middleware/authMiddleware'
 const PORT = 3000
 
 const app = express()
-app.use(express.json());
+app.use(express.json())
 app.use(cors())
-app.use("/tasks", tasksRoute)
-app.use("/user", userRoute)
-
-
+app.use(authMiddleware)
+app.use('/tasks', tasksRoute)
+app.use('/user', userRoute)
 
 app.listen(PORT, () => {
-    console.log("server on")
+  console.log('server on')
 })
