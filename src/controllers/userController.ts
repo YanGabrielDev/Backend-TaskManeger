1e85a8110189be948cabf8b18d103d56c0287b90
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2024-01-04 22:04:59 -0300

    formatando e refatorando as controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,26 +1,44 @@
-import { Request, Response } from "express";
-import { UserService } from "../services/userService";
-import { ApiError, BadRequestError } from "../erros";
-import { UserCollection } from "../models/mongo";
+import { Request, Response } from 'express'
+import { UserService } from '../services/userService'
+import { ApiError, BadRequestError } from '../erros'
+import jwt from 'jsonwebtoken'
+import { randomId } from '../utils/genereteRandomId'
 
-export class UserController{
-     public createUser = async (req: Request, res: Response) => {
-      try { 
-        const { name, email, password, confirmPassword} = await req.body
-        
-        await UserService.validateUserCreation(email, password, confirmPassword)
-        const user = UserService.createUser(name, email, password)
-        res.json(user);
-      } catch (error) {
-        if(error instanceof BadRequestError){
-          res.status(400).send({message: error.message})
-          return;
-        }
-        if(error instanceof ApiError){
-          res.status(500).send({message: error.message})
-          return;
-        }
-      
+export class UserController {
+  public createUser = async (req: Request, res: Response) => {
+    try {
+      const { name, email, password, confirmPassword } = await req.body
+
+      await UserService.validateUserCreation(email, password, confirmPassword)
+      const user = UserService.createUser(name, email, password)
+      res.json(user)
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        res.status(400).send({ message: error.message })
+        return
+      }
+      if (error instanceof ApiError) {
+        res.status(500).send({ message: error.message })
+        return
       }
     }
+  }
+
+  public signIn = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body
+      const user = UserService.getUser(email, password)
+      if (!user) {
+        throw new BadRequestError('Usuario não encontrado!')
+      }
+
+      const token = jwt.sign(req.body, randomId(), {
+        expiresIn: '1 days',
+      })
+
+      res.status(200).send({ access_token: token })
+    } catch (error) {
+      throw new ApiError('Erro do servidor!')
+    }
+  }
 }
