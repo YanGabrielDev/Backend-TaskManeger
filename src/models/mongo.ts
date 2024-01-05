f17fd789df1383be2db7ba49b82bb2a24eb0347c
Yan_Gabriel <99893016+YanGabrielDev@users.noreply.github.com>
2024-01-04 22:06:50 -0300

    formatando arquivo de model

--- src/models/mongo.ts
+++ src/models/mongo.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
-mongoose.connect('mongodb://localhost:27017/tasks');
+mongoose.connect('mongodb://localhost:27017/tasks')
 
 const TaskSchema = new mongoose.Schema({
   title: String,
   description: String,
-  completed: Boolean
-});
+  completed: Boolean,
+})
 
 const UsersSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String
-});
-const TaskCollection = mongoose.model('tasks', TaskSchema);
+  password: String,
+})
+const TaskCollection = mongoose.model('tasks', TaskSchema)
 const UserCollection = mongoose.model('users', UsersSchema)
-export {TaskCollection, UserCollection};
+export { TaskCollection, UserCollection }
