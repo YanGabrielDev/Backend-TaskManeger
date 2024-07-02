3021f2ea7b88f90d948e175d89981eab5b052487
Yan_Gabriel <yangabriel2012@gmail.com>
2024-07-01 23:19:30 -0400

    adicionando cookie parser

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import tasksRoute from './routes/tasksRoute'
 import userRoute from './routes/userRoute'
 import cors from 'cors'
 import { authMiddleware } from './middleware/authMiddleware'
-
+import cookieParser from 'cookie-parser'
 const PORT = 3005
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://localhost:3000', // ou qualquer outra porta que seu front-end esteja rodando
+  credentials: true
+}));
 app.use(authMiddleware)
 app.use('/tasks', tasksRoute)
 app.use('/user', userRoute)
