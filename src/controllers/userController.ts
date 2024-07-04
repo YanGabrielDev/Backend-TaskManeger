001532c6432e5fdf273adc4a051ff30064a6529b
Yan_Gabriel <yangabriel2012@gmail.com>
2024-07-03 20:09:20 -0400

    corrigindo erro de dominio route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -37,7 +37,10 @@ export class UserController {
       const accessToken = jwt.sign(req.body, randomId(), {
         expiresIn: '1 days',
       })
-      res.cookie('access_token', accessToken, { domain: 'study-module-one.vercel.app', secure: true, sameSite: 'none' });
+      res.cookie('access_token', accessToken, {
+        httpOnly: true,
+        domain: 'study-module-one.vercel.app'
+      });
       res.status(200).send({ message: 'Login realizado com sucesso!' })
 
     } catch (error) {
